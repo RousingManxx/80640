0416722d570f3407b7d2cbe45cbe92a287cfb013
Rousing <zs20018188@estudiantes.uv.mx>
2022-11-03 23:49:40 -0600

    Axios

--- practica06/src/main/java/mx/uv/c80640/App.java
+++ practica06/src/main/java/mx/uv/c80640/App.java
@@ -1,4 +1,7 @@
 package mx.uv.c80640;
+import static spark.Spark.*;
+
+import com.google.gson.JsonObject;
 
 /**
  * Hello world!
@@ -8,6 +11,37 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "\n\nHello World!\n\n" );
+
+        // AXIOS
+        options("/*", (request, response) -> {
+            String accessControlRequestHeaders = request.headers("Access-Control-Request-Headers");
+            if (accessControlRequestHeaders != null) {
+                response.header("Access-Control-Allow-Headers", accessControlRequestHeaders);
+            }
+            String accessControlRequestMethod = request.headers("Access-Control-Request-Method");
+            if (accessControlRequestMethod != null) {
+                response.header("Access-Control-Allow-Methods", accessControlRequestMethod);
+            }
+            return "OK";
+        });
+        before((request, response) -> response.header("Access-Control-Allow-Origin", "*"));
+        // 
+        
+        get("/hello", (req, res) -> "Hello World");
+        get("/eduardo", (req, res) -> "Hello Eduardo");
+        get("/alex", (req, res) -> "Hello Alex");
+        get("/", (req, res) -> "<h1>Bienvenido</h1><img src = 'https://www.uv.mx/v2/images/logouv.jpg'>");
+
+        post("/", (req, res) -> {
+            System.out.println(req.queryParams("email") + " " + req.queryParams("password"));
+            System.out.println(req.body());
+            res.status (200); //Codigo de respuesta
+            JsonObject oRespuesta = new JsonObject ();
+            oRespuesta.addProperty("msj", "Hola");
+            oRespuesta.addProperty("email",req.queryParams("email"));
+            return oRespuesta;
+        }); 
+
     }
 }
